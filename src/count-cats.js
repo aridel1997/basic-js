58a6d1e35163297c47e3722c1ed626beba69cdc0
aridel1997 <arinadelendik1997@gmail.com>
2021-03-14 08:04:44 +0300

    feat: add algorithms

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let newArray = matrix.flat(Infinity);
+  let counter = 0;
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] === '^^') {
+      counter++;
+    }
+  }
+  return (counter);
+
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(names) {
+  if (names === null || names === undefined) {
+    return false;
+  }
+  let arrayNames = [];
+  arrayNames = names;
+  let f = [];
+  let str = [];
+  for (let i = 0; i < arrayNames.length; i++) {
+    if (typeof (arrayNames[i]) === 'string') {
+      f = arrayNames[i].trimStart().split("");
+      str.push(f[0]);
+      str.sort(function (a, b) {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+      });
+    }
+  }
+  str = str.join("").toUpperCase();
+  if (str.length === 0) {
+    return false;
+  }
+  else {
+    return str;
+  }
+
 };
