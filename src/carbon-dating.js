7251dd3a65d96a1446c06065012013896818ff10
aridel1997 <arinadelendik1997@gmail.com>
2021-03-14 22:39:50 +0300

    feat: add algorithms

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || /[A-Z -]/gi.test(sampleActivity) || sampleActivity.trimStart().length == 0 || sampleActivity <= 0 || sampleActivity > HALF_LIFE_PERIOD || sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  let ln2 = 0.693;
+  let k = ln2 / HALF_LIFE_PERIOD;
+  let t = (Math.log(MODERN_ACTIVITY / sampleActivity)) / k;
+  return Math.ceil(t);
+
 };
